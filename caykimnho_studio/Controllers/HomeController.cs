0e1e7816b2d62b9f1b1cfb7d41287b9c618012b6
Dang Van Tuan <tuan.197pm21996@vanlanguni.vn>
2023-09-15 00:37:04 +0700

    Add project files.

---
+++ caykimnho_studio/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using caykimnho_studio.Models;
+
+namespace caykimnho_studio.Controllers
+{
+    public class HomeController : Controller
+    {
+        caykimnhoEntities model = new caykimnhoEntities();
+        public ActionResult Index()
+        {
+            var lstProduct = model.Products.ToList();
+            Session["lst-category"] = model.Categories.ToList();
+            return View("Index", lstProduct);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+    }
+}
---
+++ caykimnho_studio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="caykimnho_studio.MvcApplication" Language="C#" %>
