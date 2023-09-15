3a540fb2e29b9cb5c68d037f0f679deb2a77bdcd
Dang Van Tuan <tuan.197pm21996@vanlanguni.vn>
2023-09-15 19:06:12 +0700

    Add function view detail product

--- caykimnho_studio/Controllers/HomeController.cs
+++ caykimnho_studio/Controllers/HomeController.cs
@@ -4,17 +4,31 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using caykimnho_studio.Models;
-
+using caykimnho_studio.CheckCarts;
 namespace caykimnho_studio.Controllers
 {
+    [CheckCartsLocal]
     public class HomeController : Controller
     {
         caykimnhoEntities model = new caykimnhoEntities();
         public ActionResult Index()
         {
             var lstProduct = model.Products.ToList();
-            Session["lst-category"] = model.Categories.ToList();
-            return View("Index", lstProduct);
+            return View("index", lstProduct);
+        }
+
+        public ActionResult ProductDetail(int id)
+        {
+            var pro = model.Products.Find(id);
+            Session["relate-product"] = model.Products.Where(p => p.ID_Category == pro.ID_Category).Take(8).ToList();
+            return View("productdetail", pro);
+        }
+
+        [HttpPost]
+        public ActionResult QuickViewProduct(int id)
+        {
+            var pro = model.Products.Find(id);
+            return PartialView("_productQuickView", pro);
         }
 
         public ActionResult About()
